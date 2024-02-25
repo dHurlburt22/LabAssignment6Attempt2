93bc4df66525d04011a5336e58adc847ebe92305
dHurlburt <hurlbu19@msu.edu>
2024-02-25 13:46:45 -0500

    Got the main code to work

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-function buttonClick(){
-    alert("hiiiii");
-  }
+
+ 
+
 
     window.onload = function() {
 
-    const click = document.getElementById('click');
+    let nameArray = ['Dakota', 'Isabella', 'Dacota', 'Napolian', 'Jojo', 'Gyro', 'Johnny', 'MrSmalls', 'Laxus', 'Gojo'];
+    let chosenName = nameArray[Math.floor(Math.random() * nameArray.length)];
 
+    const click = document.getElementById('click');
     click.addEventListener("click", (event) => {
-      buttonClick();
+      buttonClick(chosenName);
+      chosenName = nameArray[Math.floor(Math.random() * nameArray.length)]
   });
 };
+
+
+function buttonClick(name){
+    alert(name);
+};
+
